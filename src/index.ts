f934e0636ea4b6ac72045753fd2e18a49bddda70
Emmanuel Abraham <sundayemmanuelabraham@gmail.com>
2025-05-09 03:21:37 +0100

    chore: added markdown documentation

--- src/index.ts
+++ src/index.ts
@@ -6,14 +6,9 @@ import http from "http";
 
 import { AgentConfig, HttpAgent, Logger, defaultSerializers } from "@risemaxi/octonet";
 import { Container } from "inversify";
-import Redis from "ioredis";
-import { Knex } from "knex";
 
 import { App } from "./app";
 import env from "./config/env";
-import { isHealthy } from "./config/health";
-import { createPostgres } from "./config/postgres";
-import { createRedis } from "./config/redis";
 import LIB_TYPES from "./internal/inversify";
 import TYPES from "@app/config/inversify.types";
 import { LinksService, LinksRepository } from "@app/links";
@@ -36,21 +31,10 @@ const start = async () => {
     const httpAgent = new HttpAgent(HTTPAgentConfig);
     container.bind<HttpAgent>(LIB_TYPES.HTTPAgent).toConstantValue(httpAgent);
 
-    // setup postgres
-    const pg = await createPostgres(logger);
-    container.bind<Knex>(LIB_TYPES.KnexDB).toConstantValue(pg);
-    logger.log("successfully connected to postgres and has run migration");
-
-    // setup in-memory store
-    const redis = await createRedis(logger);
-    container.bind<Redis>(LIB_TYPES.Redis).toConstantValue(redis);
-
-    logger.log("successfully connected to redis");
-
     container.bind<LinksService>(TYPES.LinksService).to(LinksService);
     container.bind<LinksRepository>(TYPES.LinksRepository).to(LinksRepository);
 
-    const app = new App(container, logger, () => isHealthy(pg, redis));
+    const app = new App(container, logger);
     const appServer = app.server.build();
 
     // start server
@@ -60,9 +44,6 @@ const start = async () => {
 
     process.on("SIGTERM", async () => {
       logger.log("exiting aplication...");
-
-      await redis.quit();
-      // knex automatically closes connections
 
       httpServer.close(() => {
         process.exit(0);
